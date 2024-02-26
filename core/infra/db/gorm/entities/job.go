f9dc26b49ac41187a5f70af424ec3cfdacf7b449
nash junior <nashjuniorjj@gmail.com>
2024-02-26 11:27:40 +0000

    feat: add gorm job entity

---
+++ core/infra/db/gorm/entities/job.go
@@ -0,0 +1,24 @@
+package entities
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Job struct {
+	Uuid uuid.UUID `gorm:"column:uuid;primaryKey"`
+
+	Id int `gorm:"autoIncrement;primaryKey"`
+
+	Name string
+
+	CreatedAt time.Time      `gorm:"type:timestamp;column:created_at;default:now()"`
+	UpdatedAt *time.Time     `gorm:"nullable;type:timestamp;column:updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at"`
+}
+
+func (u *Job) TableName() string {
+	return "public.jobs"
+}
